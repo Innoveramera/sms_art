2ecff691f4f0e82ac9418acecd189c9a6d23fd73
Fredrik Hammarström <fredrik@innoveramera.se>
2024-01-12 13:06:29 +0100

    .

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ async function getResponse (sms) {
         writer.on('error', reject);
     });
 
-    message = "Skicka ett SMS med önskad bild till 076-686 04 71 \n (Nu visas \"" + sms + "\")";
+    message = "Skicka ett SMS med önskad bild till " + process.env.ELKS_NR + " (Nu visas \"" + sms + "\")";
     fs.writeFile('message.txt', message, (err) => {
         if (err) {
             console.error('Error writing to file:', err);
